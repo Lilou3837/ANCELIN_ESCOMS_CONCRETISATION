4a3b8025f95642c00d8ce5583103decea5eecf7c
Ancelin Cyril <cancelin@fixe-h113-04>
2025-04-30 13:53:25 +0200

    Lecture d'une instance

---
+++ LectureInstance.py
@@ -0,0 +1,59 @@
+from cards import *
+
+def Lecture(chemin):
+    """
+    Lis une instance et renvoie les 2 tableaux de régions et de sanctuaires
+    chemin : str, nom du fichier.txt
+    """
+    fichier = open(chemin, "r")
+    chaine=fichier.read()
+    TAB = chaine.split(' ')
+    Region = []
+    Sanctuaire = []
+    for n in TAB:
+        if int(n) < 90:
+            Region.append(int(n))
+        else:
+            Sanctuaire.append(int(n))
+
+    fichier.close()
+    return Region, Sanctuaire
+
+
+print(Lecture('15_9.txt'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
