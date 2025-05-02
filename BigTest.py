ac874bb86b1f2a1c8df78ba25c4d910db4ba0ecd
XstarCyan <cyril.ancel1@gmail.com>
2025-05-02 17:28:00 +0200

    Big Test encore

--- BigTest.py
+++ BigTest.py
@@ -1,60 +1,53 @@
-from count import comptage, point, ressource
-from cards import * 
+from time import perf_counter
+start = perf_counter()
+from count import comptage
 from LectureInstance import *
-from time import *
+
 
 
 R,S=Lecture('test.txt')
 
 #Score=comptage(R,S)
-
 #print(Score)
 
 from itertools import permutations, combinations
 
-maximum = [42,42,0]
-start = perf_counter()
-"""
-for re in permutations(R,8):
-    if perf_counter() - start >= 59.75: break
-    couples_ordonnais = 0
-    for i in range(1,len(re)):
-        if re[i]>re[i-1] : couples_ordonnais += 1
+"""def permutliste(seq):
+#Retourne la liste de toutes les permutations de la liste seq (non récursif).
+    p = [seq]
+    n = len(seq)
+    for k in range(n-1):
+        for i in range(len(p)):
+            z = list(p[i])[:]
+            for c in range(n-k-1):
+                z.append(z.pop(k))
+                p.append(z[:])
+    return p"""
 
-    for san in combinations(S,couples_ordonnais):
+
+maximum = [42,42,0]
+combi_sanctu=[combinations(S,i) for i in range(0,8)]
+
+permu_region=permutations(R,8)
+
+
+s=0
+for re in permu_region:
+    s+=1
+    if perf_counter() - start >= 125.05: break
+
+    couples_ordonnais = 0
+    for i in range(1,8):
+        if re[i]>re[i-1] : couples_ordonnais += 1
+    #if couples_ordonnais>4:
+    for san in combi_sanctu[couples_ordonnais]:
+        
         pts = comptage(re,san)
         if pts > maximum[2]:
             maximum = [re, san, pts]
-"""        
 
 end = perf_counter()
-
-print(maximum, end - start)
-
-
-l=[1,2,3,4,5,6]
-for e in permutations(l,3):
-    print(e)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(maximum, end - start,s)
 
 
 
