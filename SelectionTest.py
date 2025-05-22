fecef760653bb54c32d61376097352c7cfe75948
Lilou Escoms <lilouescoms@gmail.com>
2025-05-22 17:23:38 +0200

    random_chelou

--- SelectionTest.py
+++ SelectionTest.py
@@ -1,5 +1,5 @@
 from cards import *
-from random import random
+from random import uniform
 
 def CollecteRess(TAB):
     ressources ={'r' : 0,
@@ -24,7 +24,7 @@ def CollecteRess(TAB):
     return ressources
 
 
-def ReductionSanctu(R, S, ns, ress, nb):
+def ReductionSanctu(R, S, ns, ress,I):
 
     if len(S) == ns: # Arrêt
         return R,S
@@ -42,46 +42,46 @@ def ReductionSanctu(R, S, ns, ress, nb):
                     for i in range(len(S)) : # Les sanctuaires
                         sanc = T[S[i]]
                         if sanc.couleur == parqqch : 
-                            if nb_cond!=0 : SCORES[i] += reg.score[parqqch]*2.7*nb/nb_cond
-                            else : SCORES[i] += reg.score[parqqch]+1.5*nb
+                            if nb_cond!=0 : SCORES[i] += reg.score[parqqch]*2.7*uniform(I[0],I[1])/nb_cond
+                            else : SCORES[i] += reg.score[parqqch]+1.5*uniform(I[0],I[1])
                         elif parqqch=='n' and sanc.nuit == 1:
-                            if nb_cond!=0 : SCORES[i] += reg.score[parqqch]*2.7*nb/nb_cond
-                            else : SCORES[i] += reg.score[parqqch]+1.5*nb
+                            if nb_cond!=0 : SCORES[i] += reg.score[parqqch]*2.7*uniform(I[0],I[1])/nb_cond
+                            else : SCORES[i] += reg.score[parqqch]+1.5*uniform(I[0],I[1])
                         elif parqqch=='i' and sanc.indice == 1:
-                            if nb_cond!=0 : SCORES[i] += reg.score[parqqch]*2.7*nb/nb_cond
-                            else : SCORES[i] += reg.score[parqqch]+1.5*nb
+                            if nb_cond!=0 : SCORES[i] += reg.score[parqqch]*2.7*uniform(I[0],I[1])/nb_cond
+                            else : SCORES[i] += reg.score[parqqch]+1.5*uniform(I[0],I[1])
                         elif parqqch in ["p", "chi", "cha"] : 
                             for m in sanc.merveille:
                                 if m==parqqch : 
-                                    if nb_cond!=0 : SCORES[i] += reg.score[parqqch]*2.7*nb/nb_cond
-                                    else : SCORES[i] += reg.score[parqqch] +1.5*nb
+                                    if nb_cond!=0 : SCORES[i] += reg.score[parqqch]*2.7*uniform(I[0],I[1])/nb_cond
+                                    else : SCORES[i] += reg.score[parqqch] +1.5*uniform(I[0],I[1])
                 for cond in reg.condition: ### CONDITION POUR SCORE  - max 3 éléments
                     for i in range(len(S)) : # Les sanctuaires
                         sanc = T[S[i]]
                         mervSanctu = sanc.merveille
                         if cond in mervSanctu:
                             if ress[cond]*10/len(R) + mervSanctu[cond] >= reg.condition[cond]:
-                                SCORES[i]+=1*nb
+                                SCORES[i]+=1*uniform(I[0],I[1])
 
         for i in range(len(S)):
             sanc = T[S[i]]
             for parqqch in sanc.score :
                 if parqqch != "a" and parqqch != "e":
                     if ress[parqqch]*10/len(R) >= sanc.score[parqqch]:
-                        SCORES[i] += sanc.score[parqqch]*nb
+                        SCORES[i] += sanc.score[parqqch]*uniform(I[0],I[1])
                 elif parqqch == 'a': # Merci la 121
-                    SCORES[i] += sanc.score[parqqch]*nb
+                    SCORES[i] += sanc.score[parqqch]*uniform(I[0],I[1])
                 else: # parqqch == 'e'
                     ens = min([ress['r']*10/len(R), ress['b']*10/len(R), ress['j']*10/len(R), ress['v']*10/len(R)])
-                    SCORES[i] += sanc.score[parqqch] * ens*nb
+                    SCORES[i] += sanc.score[parqqch] * ens*uniform(I[0],I[1])
         
         S.pop(SCORES.index(min(SCORES)))
         SCORES.pop(SCORES.index(min(SCORES)))
 
-        return ReductionSanctu(R, S, ns, ress,nb)
+        return ReductionSanctu(R, S, ns, ress,I)
 
 
-def ReductionRegion(R, S, nr, ressS,nb):
+def ReductionRegion(R, S, nr, ressS,I):
 
     if len(R) == nr: # Arrêt
         return R,S
@@ -97,15 +97,15 @@ def ReductionRegion(R, S, nr, ressS,nb):
                     for i in range(len(R)) : # Les régions qui satisferont peut etre ce que demandent les sanctuaires pour leur score
                         reg = T[R[i]]
                         if reg.couleur == parqqch : 
-                            SCORES[i] += sanc.score[parqqch]+1.5*nb
+                            SCORES[i] += sanc.score[parqqch]+1.5*uniform(I[0],I[1])
                         elif parqqch=='n' and reg.nuit == 1:
-                            SCORES[i] += sanc.score[parqqch] +1.5*nb
+                            SCORES[i] += sanc.score[parqqch] +1.5*uniform(I[0],I[1])
                         elif parqqch=='i' and reg.indice == 1:
-                            SCORES[i] += sanc.score[parqqch] + 1.5*nb
+                            SCORES[i] += sanc.score[parqqch] + 1.5*uniform(I[0],I[1])
                         elif parqqch in ["p", "chi", "cha"] : 
                             for m in reg.merveille:
                                 if m==parqqch : 
-                                    SCORES[i] += sanc.score[parqqch] *nb
+                                    SCORES[i] += sanc.score[parqqch] *uniform(I[0],I[1])
         for i in range(len(R)):
             reg = T[R[i]]         # Régions qui donnent des "conditions" que les autres doivent vérifier
             for parqqch in reg.score :
@@ -114,16 +114,16 @@ def ReductionRegion(R, S, nr, ressS,nb):
                     nb_cond += 1 #reg.condition[c] 
                 if parqqch != "a" and parqqch != "e":
                     if ressS[parqqch]*7/len(S) + ressR[parqqch]*10/len(R) >= reg.score[parqqch]:
-                        SCORES[i] += reg.score[parqqch]*nb
+                        SCORES[i] += reg.score[parqqch]*uniform(I[0],I[1])
                 elif parqqch == 'a':
-                    if nb_cond!=0: SCORES[i] += reg.score[parqqch]*2.7*nb/nb_cond
-                    else: SCORES[i] += reg.score[parqqch]+1.5*nb
+                    if nb_cond!=0: SCORES[i] += reg.score[parqqch]*2.7*uniform(I[0],I[1])/nb_cond
+                    else: SCORES[i] += reg.score[parqqch]+1.5*uniform(I[0],I[1])
                 else: # parqqch == 'e'
                     ens = min([ressS['r']*7/len(S)+ressR['r']*10/len(R), 
                                 ressS['b']*7/len(S)+ressR['b']*10/len(R), 
                                 ressS['j']*7/len(S)+ressR['j']*10/len(R), 
                                 ressS['v']*7/len(S)+ressR['v']*10/len(R)])
-                    SCORES[i] += reg.score[parqqch] * ens*nb
+                    SCORES[i] += reg.score[parqqch] * ens*uniform(I[0],I[1])
 
             if reg.condition != {}:
                 for cond in reg.condition: ### CONDITION POUR SCORE DES REGIONS  - max 3 éléments
@@ -132,50 +132,47 @@ def ReductionRegion(R, S, nr, ressS,nb):
                         mervRegion = reg2.merveille
                         if cond in mervRegion:
                             if ressR[cond]*10/len(R) + ressS[cond]*7/len(S) + mervRegion[cond] >= reg.condition[cond]:
-                                SCORES[j]+=1*nb
+                                SCORES[j]+=1*uniform(I[0],I[1])
             else:
                 SCORES[i] += 1
         
         R.pop(SCORES.index(min(SCORES)))
         SCORES.pop(SCORES.index(min(SCORES)))
 
-        return ReductionRegion(R, S, nr, ressS,nb)
+        return ReductionRegion(R, S, nr, ressS,I)
 
-def ReductionTotale(R, S, nr, ns,nb):
+def ReductionTotale(R, S, nr, ns,I):
     if len(R)==nr and len(S)==ns:
         return R,S 
     elif len(R)==nr:
         RESS=CollecteRess(R)
-        return ReductionSanctu(R,S,ns,RESS,nb)
+        return ReductionSanctu(R,S,ns,RESS,I)
     elif len(S)==ns:
         RESS=CollecteRess(S)
-        return ReductionRegion(R,S,nr,RESS,nb)
+        return ReductionRegion(R,S,nr,RESS,I)
     else:
         RESS=CollecteRess(S)
-        R,S = ReductionRegion(R,S,len(R)-1,RESS,nb)
+        R,S = ReductionRegion(R,S,len(R)-1,RESS,I)
         RESS=CollecteRess(R)
-        R,S = ReductionSanctu(R,S,len(S)-1,RESS,nb)
-        return ReductionTotale(R,S,nr,ns,nb) 
+        R,S = ReductionSanctu(R,S,len(S)-1,RESS,I)
+        return ReductionTotale(R,S,nr,ns,I) 
 
 
 
-def Selection(R, S):
+def Selection(R, S,I):
     """
     R : Régions, S : Sanctuaires
     """
 
     # Conditions pour savoir vers quoi on va
-    nb = random.random()
-    if len(R) == 8 and len(S) >= 11:
+    
+    if len(R) == 8 and len(S) >= 8:
         RESS=CollecteRess(R)
-        return ReductionSanctu(R,S,10,RESS,nb)
-    elif len(R) == 9 and len(S) >= 8:
-        RESS=CollecteRess(R)
-        return ReductionSanctu(R,S,7,RESS,nb)
-    elif len(R) >=10 and len(S) ==7:
+        return ReductionSanctu(R,S,7,RESS,I)
+    elif len(R) >=9 and len(S) ==7:
         RESS=CollecteRess(S)
-        return ReductionRegion(R,S,9,RESS,nb)
+        return ReductionRegion(R,S,8,RESS,I)
     elif len(R) >= 9 and len(S) >= 8:
-        return ReductionTotale(R,S,9,7)
+        return ReductionTotale(R,S,8,7,I)
     else: 
         return R,S # Part dans RechercheGlouton
--- maintest.py
+++ maintest.py
@@ -1,22 +1,37 @@
 from time import perf_counter
 start=perf_counter()
 from LectureInstance import *
-from SelectionCoef import Selection
+from SelectionTest import Selection
 from Recherche import RecherchePresqueGlouton
-from copy import deepcopy
+from random import uniform
 
 
 
 def FonctionMere(instance: str):
     R,S = Lecture(instance)
 
-    R0 = deepcopy(R)
-    S0 = deepcopy(S)
+    R0 = [k for k in R]
+    S0 = [k for k in S]
     NomFichier = f"{instance[13:-4]}_result.txt"
     m=[42,42,0]
-    R,S = Selection(R,S)
-    Max, temps = RecherchePresqueGlouton(R,S)
-    Ecriture(f'Competitions_Results/{NomFichier}', (R0, S0), (Max, temps))
+    new=m
+    b=uniform(6,10)
+    print(b)
+    while perf_counter()-start <59 : 
+        #nb = uniform(0,10)
+        I=[1,10]
+        R1=[k for k in R0]#deepcopy(R0)
+        S1=[k for k in S0]
+        R,S = Selection(R1,S1,I)
+        
+        Max, temps = RecherchePresqueGlouton(R,S)
+        if m!=new :
+            new=m
+            print('new :',m[2])
+        if Max[2]>m[2] : m=[k for k in Max]
+    
+    Ecriture(f'Competitions_Results/{NomFichier}', (R0, S0), (m, temps))
+    print( perf_counter()-start)
 
 
 FonctionMere('Competitions/competition_15.txt')
